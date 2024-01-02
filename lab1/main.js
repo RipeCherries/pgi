e436396cce6828d0bab72b896437cdca60705fce
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-01-02 21:33:24 +0700

    Реализовал вывод информации о BMP файле

---
+++ lab1/main.js
@@ -0,0 +1,47 @@
+import chalk from "chalk";
+import { promisify } from "util";
+import fs from "fs";
+
+const readFileAsync = promisify(fs.readFile);
+
+const decodingCompressionField = {
+    0: "BL_RGB | Двумерный массив",
+    1: "BL_RLE8 | RLE кодирование",
+    2: "BL_RLE4 | RLE кодирование",
+    3: "BI_BITFIELDS | двумерный массив с масками цветовых каналов",
+    4: "BI_JPEG | во встроенном JPEG-файле",
+    5: "BI_PNG | во встроенном PNG-файле",
+    6: "BI_ALPHABITFIELDS | двумерный массив с масками цветовых и альфа-канала"
+}
+
+async function getBMPFileInformation(fileName) {
+    try {
+        const bmpData = await readFileAsync(fileName);
+
+        return {
+            bfType: bmpData.readUInt16LE(0),
+            bfSize: bmpData.readUInt32LE(2),
+            biWidth: bmpData.readUInt32LE(18),
+            biHeight: bmpData.readUInt32LE(22),
+            biBitCount: bmpData.readUInt16LE(28),
+            biCompression: bmpData.readUInt32LE(30),
+            biXPelsPerMeter: bmpData.readUInt32LE(38),
+            biYPelsPerMeter: bmpData.readUInt32LE(42)
+        }
+    } catch (error) {
+
+    }
+}
+
+getBMPFileInformation("example.bmp").then((bmpInfo) => {
+    console.log(chalk.green.underline("Сигнатура формата:") + " " + bmpInfo.bfType.toString(16));
+    console.log(chalk.green.underline("Размер файла:") + " " + bmpInfo.bfSize + " байт");
+    console.log(chalk.green.underline("Ширина:") + " " + bmpInfo.biWidth + " пикселей");
+    console.log(chalk.green.underline("Высота:") + " " + bmpInfo.biHeight + " пикселей");
+    console.log(chalk.green.underline("Количество бит на пиксель:") + " " + bmpInfo.biBitCount);
+    console.log(chalk.green.underline("Способ хранения пикселей:") + " " + decodingCompressionField[bmpInfo.biCompression]);
+    console.log(chalk.green.underline("Разрешение по горизонтали:") + " " + bmpInfo.biXPelsPerMeter + " пикселей / метр");
+    console.log(chalk.green.underline("Разрешение по вертикали:") + " " + bmpInfo.biYPelsPerMeter + " пикселей / метр");
+})
+
+
