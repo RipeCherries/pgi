296aa8c14385f6584136c18692082a1f2d239390
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-02-10 16:07:00 +0700

    Переделал 7 лабораторную работу в соответствии с новым заданием

--- lab7/main.js
+++ lab7/main.js
@@ -5,31 +5,53 @@ import chalk from 'chalk';
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 
-async function writeTextToBMP(bmpFileName, textFileName) {
+const mask = {
+  10: [1, 1],
+  20: [3, 2],
+  50: [15, 4],
+};
+
+async function writeTextToBMP(bmpFileName, textFileName, percents) {
   const bmpData = await readFileAsync(bmpFileName);
   const textData = await readFileAsync(textFileName);
 
   const pixelsDataOffset = bmpData.readUInt32LE(10);
 
-  if (textData.length * 8 > bmpData.length - pixelsDataOffset) {
-    throw new Error('Текстовый файл превышает максимально допустимый размер для этого изображения');
-  }
-
-  const textBits = textData.reduce(
+  let textBits = textData.reduce(
     (previousValue, currentValue) => previousValue + currentValue.toString(2).padStart(8, '0'),
     '',
   );
 
-  const textBitsSize = textBits.length.toString(2).padStart(32, '0');
+  textBits += '00000000';
 
-  for (let i = 0; i < textBitsSize.length; i++) {
-    bmpData[i + pixelsDataOffset] &= ~1;
-    bmpData[i + pixelsDataOffset] |= parseInt(textBitsSize[i], 2);
-  }
+  for (let j = 0; j < bmpData.readUInt32LE(22); ++j) {
+    for (let i = 0; i < bmpData.readUInt32LE(18); ++i) {
+      const offset = pixelsDataOffset + (j * bmpData.readUInt32LE(18) + i) * 3;
 
-  for (let i = 0; i < textBits.length; i++) {
-    bmpData[i + textBitsSize.length + pixelsDataOffset] &= ~1;
-    bmpData[i + textBitsSize.length + pixelsDataOffset] |= parseInt(textBits[i], 2);
+      if (textBits.length > 0) {
+        bmpData[offset] &= ~mask[percents][0];
+        bmpData[offset] |= parseInt(textBits.substring(0, mask[percents][1]), 2);
+        textBits = textBits.slice(mask[percents][1]);
+      } else {
+        break;
+      }
+
+      if (textBits.length > 0) {
+        bmpData[offset + 1] &= ~mask[percents][0];
+        bmpData[offset + 1] |= parseInt(textBits.substring(0, mask[percents][1]), 2);
+        textBits = textBits.slice(mask[percents][1]);
+      } else {
+        break;
+      }
+
+      if (textBits.length > 0) {
+        bmpData[offset + 2] &= ~mask[percents][0];
+        bmpData[offset + 2] |= parseInt(textBits.substring(0, mask[percents][1]), 2);
+        textBits = textBits.slice(mask[percents][1]);
+      } else {
+        break;
+      }
+    }
   }
 
   await writeFileAsync('output/stenography.bmp', bmpData);
@@ -37,26 +59,33 @@ async function writeTextToBMP(bmpFileName, textFileName) {
   return 'Текст успешно вписан в изображение!';
 }
 
-async function readTextFromBMP(bmpFileName) {
+async function readTextFromBMP(bmpFileName, percents) {
   const bmpData = await readFileAsync(bmpFileName);
   const pixelsDataOffset = bmpData.readUInt32LE(10);
 
-  const textBitsSize = bmpData
-    .subarray(pixelsDataOffset, pixelsDataOffset + 32)
-    .map((byte) => byte & 1)
-    .join('');
-  const textSize = parseInt(textBitsSize, 2);
+  let readText = '';
 
-  let textBits = '';
+  for (let j = 0; j < bmpData.readUInt32LE(22); ++j) {
+    for (let i = 0; i < bmpData.readUInt32LE(18); ++i) {
+      const offset = pixelsDataOffset + (j * bmpData.readUInt32LE(18) + i) * 3;
 
-  for (let i = 0; i < textSize; i++) {
-    textBits += (bmpData[i + pixelsDataOffset + 32] & 1).toString();
+      readText += (bmpData[offset] & mask[percents][0]).toString(2).padStart(mask[percents][1], '0');
+      readText += (bmpData[offset + 1] & mask[percents][0]).toString(2).padStart(mask[percents][1], '0');
+      readText += (bmpData[offset + 2] & mask[percents][0]).toString(2).padStart(mask[percents][1], '0');
+    }
   }
 
-  const textBuffer = Buffer.alloc(textSize / 8);
+  for (let i = 0; i < readText.length; i += 8) {
+    if (readText.slice(i, i + 8) === '00000000') {
+      readText = readText.slice(0, i);
+      break;
+    }
+  }
 
-  for (let i = 0; i < textSize; i += 8) {
-    const byte = parseInt(textBits.slice(i, i + 8), 2);
+  const textBuffer = Buffer.alloc(readText.length / 8);
+
+  for (let i = 0; i < readText.length; i += 8) {
+    const byte = parseInt(readText.slice(i, i + 8), 2);
     textBuffer.writeUInt8(byte, i / 8);
   }
 
@@ -67,11 +96,11 @@ async function readTextFromBMP(bmpFileName) {
   return 'Извлечённый из изображения текст успешно записан в файл!';
 }
 
-writeTextToBMP('input/example24bit.bmp', 'input/text.txt')
+writeTextToBMP('input/example24bit.bmp', 'input/text.txt', 50)
   .then((writeResult) => {
     console.log(chalk.white.bold(writeResult));
 
-    readTextFromBMP('output/stenography.bmp')
+    readTextFromBMP('output/stenography.bmp', 50)
       .then((readResult) => {
         console.log(chalk.white.bold(readResult));
       })
